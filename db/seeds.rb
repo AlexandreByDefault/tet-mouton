e8df272f639a8f51c88ccc672c3dc7e4318286ca
barbara boston <b_bellegarde@hotmail.com>
2022-03-15 15:20:31 -0400

    seed mouton

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+farmer2 = User.create(name: "Tartampion", firstname:"Serge", email: "tartes@gmail.com", password:"5678")
+# propriétaires
+owner2 = User.create(name: "Boisjoli", firstname:"Eustache", email: "boiseu@gmail.com", password:"1234")
+# terrains
+land2 = Land.create(location: "gros-morne", area: "400m²", price: 900)
+# réservations
+booking2 = Booking.create(land2, farmer2)
