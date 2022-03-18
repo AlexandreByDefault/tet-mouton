2398257ecd74de387b126016eff8ae72aa3bd5be
Moïse GUANNEL <mguannel97@gmail.com>
2022-03-18 18:00:59 +0100

    updates

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_15_201004) do
+ActiveRecord::Schema.define(version: 2022_03_17_165632) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2022_03_15_201004) do
     t.bigint "owner_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "photo_url"
     t.index ["owner_id"], name: "index_lands_on_owner_id"
   end
 
